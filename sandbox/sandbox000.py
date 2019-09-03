62d5bf5bdf4f78aed7bcfb79418f943c03414333
Sharif M. Tarik <s.tarik@cimplux.com>
2019-09-03 15:52:47 +0600

    Add unary real numbers and tests

--- sandbox/sandbox000.py
+++ sandbox/sandbox000.py
@@ -7,6 +7,25 @@ import pyparsing as pp
 
 
 # Real number -----------------------------------------------------------------
+real_number_tests = (
+    '''
+    42
+    42.69
+    0.42
+    .42
+    -2
+    -.45
+    -1.34
+    
+    # to fail:
+    
+    42..
+    ..42
+    42..69
+    '''
+)
+
+
 def to_int_or_decimal(number: str) -> Union[int, Decimal]:
     """ParseAction to convert a string into an integer or decimal."""
     return Decimal(number) if '.' in number else int(number)
@@ -14,8 +33,10 @@ def to_int_or_decimal(number: str) -> Union[int, Decimal]:
 
 real_number_type_1 = pp.Combine(pp.Word(pp.nums) + pp.Optional('.' + pp.Word(pp.nums)))  # 1, 1.23
 real_number_type_2 = pp.Combine(pp.Optional(pp.Word(pp.nums)) + '.' + pp.Word(pp.nums))  # .1, 2.1
-real_number = real_number_type_1 | real_number_type_2
+real_number = pp.Combine(pp.Optional(pp.oneOf('+ -')) + (real_number_type_1 | real_number_type_2))
 real_number.setParseAction(lambda toks: to_int_or_decimal(toks[0]))
+
+real_number.runTests(real_number_tests)
 # ---
 
 
