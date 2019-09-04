45d39671e4513b915f02c226033d2e91fabf9979
Sharif M. Tarik <s.tarik@cimplux.com>
2019-09-04 13:54:33 +0600

    Reorder tests to be run when __main__

--- sandbox/sandbox000.py
+++ sandbox/sandbox000.py
@@ -7,25 +7,6 @@ import pyparsing as pp
 
 
 # Real number -----------------------------------------------------------------
-real_number_tests = (
-    '''
-    42
-    42.69
-    0.42
-    .42
-    -2
-    -.45
-    -1.34
-    
-    # to fail:
-    
-    42..
-    ..42
-    42..69
-    '''
-)
-
-
 def to_int_or_decimal(number: str) -> Union[int, Decimal]:
     """ParseAction to convert a string into an integer or decimal."""
     return Decimal(number) if '.' in number else int(number)
@@ -35,8 +16,6 @@ real_number_type_1 = pp.Combine(pp.Word(pp.nums) + pp.Optional('.' + pp.Word(pp.
 real_number_type_2 = pp.Combine(pp.Optional(pp.Word(pp.nums)) + '.' + pp.Word(pp.nums))  # .1, 2.1
 real_number = pp.Combine(pp.Optional(pp.oneOf('+ -')) + (real_number_type_1 | real_number_type_2))
 real_number.setParseAction(lambda toks: to_int_or_decimal(toks[0]))
-
-real_number.runTests(real_number_tests)
 # ---
 
 
@@ -47,3 +26,26 @@ named_variable = pp.Combine(
     + pp.Optional(pp.Word(pp.alphanums + '_'))
 )
 # ---
+
+
+if __name__ == '__main__':
+    real_number_tests = (
+        '''
+        # real number tests:
+        
+        42
+        42.69
+        0.42
+        .42
+        -2
+        -.45
+        -1.34
+
+        # to fail:
+
+        42..
+        ..42
+        42..69
+        '''
+    )
+    real_number.runTests(real_number_tests)
