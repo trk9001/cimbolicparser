c27cd6e0b33507fc13ac02b9f14d941efc12ce52
trk <dev.trk.9001@gmail.com>
2019-09-07 20:42:14 +0600

    Simplify and update sandbox000

--- sandbox/sandbox000.py
+++ sandbox/sandbox000.py
@@ -1,26 +1,29 @@
 """The culmination of experiment results."""
 
 from decimal import Decimal
-from typing import Union
+from typing import Iterable, Union
 
 import pyparsing as pp
 
 
-# Real number -----------------------------------------------------------------
-def to_int_or_decimal(number: str) -> Union[int, Decimal]:
-    """ParseAction to convert a string into an integer or decimal."""
-    return Decimal(number) if '.' in number else int(number)
+def evaluator(tokens: Union[str, Iterable]) -> Union[int, Decimal]:
+    """Evaluate a string or a ParseResult containing a reduced expression."""
+    if not isinstance(tokens, str):
+        tokens = ''.join([str(tok) for tok in tokens])
+    result = eval(tokens)
+    return result
 
 
+# Real number grammar ---------------------------------------------------------
 real_number_type_1 = pp.Combine(pp.Word(pp.nums) + pp.Optional('.' + pp.Word(pp.nums)))
 real_number_type_2 = pp.Combine(pp.Optional(pp.Word(pp.nums)) + '.' + pp.Word(pp.nums))
 real_number = pp.Combine(pp.Optional(pp.oneOf('+ -')) + (real_number_type_1 | real_number_type_2))
-real_number.setParseAction(lambda toks: to_int_or_decimal(toks[0]))
+real_number.setParseAction(lambda toks: Decimal(toks[0]) if '.' in toks[0] else int(toks[0]))
 # ---
 
 
-# Named variable --------------------------------------------------------------
-# Note: This needs a ParseAction for conversion to a numeric value.
+# Named variable grammar ------------------------------------------------------
+# TODO: Add a ParseAction for conversion to a numerical value
 named_variable = pp.Combine(
     pp.Suppress('$')
     + pp.Word(pp.alphas + '_', exact=1)
@@ -29,46 +32,72 @@ named_variable = pp.Combine(
 # ---
 
 
-# Arithmetic operator ---------------------------------------------------------
+# Arithmetic operator grammar -------------------------------------------------
 multiplicative_arithmetic_operator = pp.oneOf('^ * / mod')
 multiplicative_arithmetic_operator.setParseAction(lambda toks: '%' if toks[0] == 'mod' else toks[0])
 additive_arithmetic_operator = pp.oneOf('+ -')
 # ---
 
 
-# Arithmetic expression -------------------------------------------------------
-expression = pp.Forward()
-term = ((real_number | named_variable | expression)
-        + pp.ZeroOrMore(multiplicative_arithmetic_operator
-                        + (real_number | named_variable | expression)))
-expression <<= (
-        (
-            '(' + term + pp.ZeroOrMore(additive_arithmetic_operator + term) + ')'
-            + pp.ZeroOrMore((multiplicative_arithmetic_operator + expression) | (additive_arithmetic_operator + term))
-        )
-        | (term + pp.ZeroOrMore(additive_arithmetic_operator + term))
+# Arithmetic expression grammar (forward-declared) ----------------------------
+arithmetic_expression = pp.Forward()
+# ---
+
+
+# Aggregate macro grammar -----------------------------------------------------
+aggregate_macro_names_allowed = 'max min'
+
+
+def aggregate_macro_evaluator(toks: Iterable) -> Union[int, Decimal]:
+    """Recursively evaluate the arguments to an aggregate macro."""
+    macro_name, macro_args = tuple(toks)
+    sub_expression = ''
+    parsed_args = []
+    for arg in macro_args:
+        if arg != ',':
+            sub_expression += str(arg)
+        else:
+            parsed_args.append(evaluator(sub_expression))
+            sub_expression = ''
+    parsed_args.append(evaluator(sub_expression))
+    result = evaluator(f'{macro_name}({str(parsed_args)})')
+    return result
+
+
+aggregate_macro_name = pp.oneOf(aggregate_macro_names_allowed)
+aggregate_macro = (
+    pp.Combine(aggregate_macro_name + pp.Suppress('('))
+    + pp.Group(arithmetic_expression + pp.ZeroOrMore(pp.Literal(',') + arithmetic_expression))
+    + pp.Suppress(')')
+)
+aggregate_macro.setParseAction(lambda toks: aggregate_macro_evaluator(toks))
+# ---
+
+
+# Arithmetic term grammar -----------------------------------------------------
+arithmetic_term = (
+    (real_number | named_variable | aggregate_macro | arithmetic_expression)
+    + pp.ZeroOrMore(
+        multiplicative_arithmetic_operator
+        + (real_number
+           | named_variable
+           | aggregate_macro
+           | arithmetic_expression)
+    )
 )
 # ---
 
 
-if __name__ == '__main__':
-    real_number_tests = (
-        '''
-        # real number tests:
-        
-        42
-        42.69
-        0.42
-        .42
-        -2
-        -.45
-        -1.34
-
-        # to fail:
-
-        42..
-        ..42
-        42..69
-        '''
+# Arithmetic expression grammar -------------------------------------------------------
+# TODO: Add rigorous testing
+arithmetic_expression <<= (
+    (
+        '(' + arithmetic_term + pp.ZeroOrMore(additive_arithmetic_operator + arithmetic_term) + ')'
+        + pp.ZeroOrMore(
+            (multiplicative_arithmetic_operator + arithmetic_expression)
+            | (additive_arithmetic_operator + arithmetic_term)
+        )
     )
-    real_number.runTests(real_number_tests)
+    | (arithmetic_term + pp.ZeroOrMore(additive_arithmetic_operator + arithmetic_term))
+)
+# ---
