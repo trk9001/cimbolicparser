dc50f83d20df2b0e763332ee4947f227be52ee36
Sharif M. Tarik <s.tarik@cimplux.com>
2019-09-08 19:22:22 +0600

    Copy sandbox results into the `cimbolic` package

---
+++ cimbolic/core.py
@@ -0,0 +1,17 @@
+from decimal import Decimal
+from typing import Iterable, Union
+
+
+def evaluator(tokens: Union[str, Iterable]) -> Union[int, Decimal]:
+    """Evaluate a string or a ParseResult containing a reduced expression."""
+    if not isinstance(tokens, str):
+        tokens = ' '.join([str(tok) for tok in tokens])
+    result = eval(tokens)
+    return result
+
+
+def print_tokens(tokens):
+    """Callable ParseAction to print tokens for debugging purposes."""
+    from pprint import pprint
+    pprint(tokens.asList())
+    return tokens
