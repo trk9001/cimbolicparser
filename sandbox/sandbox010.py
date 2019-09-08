506d262a8b7acf2936ab4e14672226c061a2d6a6
Sharif M. Tarik <s.tarik@cimplux.com>
2019-09-08 17:42:39 +0600

    Add strings and tests for the cond eval (sb010)

--- sandbox/sandbox010.py
+++ sandbox/sandbox010.py
@@ -23,9 +23,7 @@ def iter_value():
 
 val_iter = iter_value()
 
-named_variable.setParseAction(lambda toks: next(val_iter))
-
-aggregate_macro.addParseAction(print_toks)
+named_variable.setParseAction(lambda toks: next(val_iter) if toks[0] != 'pro' else '"pro"')
 
 
 conditional_operator_equality = pp.oneOf('== !=')
@@ -41,9 +39,9 @@ null = pp.Literal('null').setParseAction(lambda toks: None)
 conditional_term = arithmetic_expression
 
 condition = (
-    ('(' + conditional_term + conditional_operator_equality + (boolean_value | null) + ')')
+    ('(' + conditional_term + conditional_operator_equality + (quoted_string | boolean_value | null) + ')')
     | ('(' + conditional_term + conditional_operator_all + conditional_term + ')')
-    | (conditional_term + conditional_operator_equality + (boolean_value | null))
+    | (conditional_term + conditional_operator_equality + (quoted_string | boolean_value | null))
     | (conditional_term + conditional_operator_all + conditional_term)
 )
 
@@ -54,29 +52,61 @@ conditional_expression <<= (
     | (logical_negation_operator + conditional_expression)
     | (condition + pp.ZeroOrMore(logical_combination_operator + conditional_expression))
 ) + pp.ZeroOrMore(logical_combination_operator + conditional_expression)
-conditional_expression.setParseAction(print_toks)
 
-conditional_expression.runTests(
-    '''
-    42 > 69
-    not (42 > 69)
-    42 < 69
-    (42 < 69)
-    42 >= 69
-    (42 >= 69)
-    42 <= 69
-    (42 <= 69)
-    42 != 69
-    (42 != 69)
-    (42 != 69) and (42<3)
-    (42 != 69) and not (42<3)
-    ((42 != 69) and (42<3))
-    ((42 != 69) and not (42<3))
-    42 != 69 and 42<3 or 45>2
-    
-    # to fail:
-    
-    42
-    (((42 != 69) and not (42<3)) or (23<4))
-    '''
-)
+# conditional_expression.runTests(
+#     '''
+#     42 > 69
+#     not (42 > 69)
+#     42 < 69
+#     (42 < 69)
+#     42 >= 69
+#     (42 >= 69)
+#     42 <= 69
+#     (42 <= 69)
+#     42 != 69
+#     (42 != 69)
+#     (42 != 69) and (42<3)
+#     (42 != 69) and not (42<3)
+#     ((42 != 69) and (42<3))
+#     ((42 != 69) and not (42<3))
+#     42 != 69 and 42<3 or 45>2
+#
+#     # to fail:
+#
+#     42
+#     (((42 != 69) and not (42<3)) or (23<4))
+#     '''
+# )
+
+if __name__ == '__main__':
+    tests = [
+        ('42 > 69', False),
+        ('not (42 > 69)', True),
+        ('42 < 69', True),
+        ('(42 < 69)', True),
+        ('42 >= 69', False),
+        ('(42 >= 69)', False),
+        ('42 <= 69', True),
+        ('(42 <= 69)', True),
+        ('42 != 69', True),
+        ('(42 != 69)', True),
+        ('(42 != 69) and (42 < 3)', False),
+        ('(42 != 69) and not (42 < 3)', True),
+        ('((42 != 69) and (42 < 3))', False),
+        ('((42 != 69) and not (42 < 3))', True),
+        ('42 != 69 and 42 < 3 or 45 > 2', True),
+        ('(42 + 69 < 420)', True),
+        ('((42 + 69) < 420)', True),
+        ('(max(1, 34, 33 + 3, (42 / 6)) < min(230, 45))', True),
+        ('3 < 29 % 5', True),
+        ('$pro == "pro"', True),
+    ]
+    for test in tests:
+        result = evaluator(conditional_expression.parseString(test[0]))
+        # from pprint import pprint
+        # pprint(result)
+        # print()
+        if result == test[1]:
+            print('SUCCESS')
+        else:
+            print('FAIL')
