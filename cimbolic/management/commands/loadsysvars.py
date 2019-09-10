842b85db22ee99d0ee18a5a5361a41a7ad6806a0
Sharif M. Tarik <s.tarik@cimplux.com>
2019-09-10 21:31:18 +0600

    Add functionality to the loadsysvars command
    Inactive but referenced variables are now marked as active.

--- cimbolic/management/commands/loadsysvars.py
+++ cimbolic/management/commands/loadsysvars.py
@@ -10,7 +10,9 @@ class Command(BaseCommand):
     It collects variables that are referenced in the cimbolicsysvars.py file
     in the project's root directory, but whose names do not exist among the
     Variable objects. It then creates Variable objects from those variables,
-    given that no clashing occurs with the existing Variable objects.
+    given that no clashing occurs with the existing Variable objects. If an
+    inactive system-defined Variable object exists (is_active == False), it
+    also marks the Variable object as active.
 
     It also marks Variable objects not referenced in cimbolicsysvars.py anymore
     as inactive (is_active = False). This means that the mentioned file is the
@@ -26,20 +28,26 @@ class Command(BaseCommand):
             if not Variable.objects.filter(name=var).exists():
                 Variable.objects.create(name=var, type=Variable.SYSTEM_DEFINED, is_active=True)
                 vars_added.append(var)
+            else:
+                v = Variable.objects.get(name=var)
+                if not v.is_active:
+                    vars_added.append(v)
+                    v.is_active = True
+                    v.save(update_fields=['is_active'])
 
         if vars_added:
-            self.stdout.write(self.style.SUCCESS('The following variables have been loaded: '), ending='')
+            self.stdout.write(self.style.SUCCESS('The following variables have been loaded or activated: '), ending='')
             for var in vars_added[:-1]:
                 self.stdout.write(self.style.SUCCESS(f'{var}'), ending=', ')
             self.stdout.write(self.style.SUCCESS(f'{vars_added[-1]}'))
 
         vars_marked_inactive = []
-        sys_vars_in_db = Variable.objects.filter(type=Variable.SYSTEM_DEFINED)
+        sys_vars_in_db = Variable.objects.filter(type=Variable.SYSTEM_DEFINED, is_active=True)
         for var in sys_vars_in_db:
             if var.name not in sys_vars:
                 vars_marked_inactive.append(var.name)
                 var.is_active = False
-                var.save()
+                var.save(update_fields=['is_active'])
 
         if vars_marked_inactive:
             self.stdout.write(self.style.SUCCESS('The following variables have been marked as inactive: '), ending='')
