4587f5dd1d00cc0769d2f791f602e83c828a92a1
Sharif M. Tarik <s.tarik@cimplux.com>
2019-09-10 19:29:43 +0600

    Switch to lowercase var name for PEP8 adherence

--- cimbolic/utils.py
+++ cimbolic/utils.py
@@ -7,9 +7,9 @@ from django.conf import settings
 
 def get_system_defined_variables() -> Dict:
     """Read the file in which system-defined variables are stored and return """
-    SYSTEM_DEFINED_VARIABLE_FILE_NAME = 'cimbolicsysvars.py'
-    file_path = os.path.join(settings.BASE_DIR, SYSTEM_DEFINED_VARIABLE_FILE_NAME)
-    module_name = ''.join(SYSTEM_DEFINED_VARIABLE_FILE_NAME.split('.')[:-1])
+    system_defined_variable_file_name = 'cimbolicsysvars.py'
+    file_path = os.path.join(settings.BASE_DIR, system_defined_variable_file_name)
+    module_name = ''.join(system_defined_variable_file_name.split('.')[:-1])
     spec = importlib.util.spec_from_file_location(module_name, file_path)
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
