bc92321b1f1acfebb92cd152939ec54b19f81f2c
Sharif M. Tarik <s.tarik@cimplux.com>
2019-09-10 19:43:59 +0600

    Turn the file name var into a module constant

--- cimbolic/utils.py
+++ cimbolic/utils.py
@@ -4,12 +4,13 @@ from typing import Dict
 
 from django.conf import settings
 
+SYSTEM_DEFINED_VARIABLE_FILE_NAME = 'cimbolicsysvars.py'
+
 
 def get_system_defined_variables() -> Dict:
     """Read the file in which system-defined variables are stored and return """
-    system_defined_variable_file_name = 'cimbolicsysvars.py'
-    file_path = os.path.join(settings.BASE_DIR, system_defined_variable_file_name)
-    module_name = ''.join(system_defined_variable_file_name.split('.')[:-1])
+    file_path = os.path.join(settings.BASE_DIR, SYSTEM_DEFINED_VARIABLE_FILE_NAME)
+    module_name = ''.join(SYSTEM_DEFINED_VARIABLE_FILE_NAME.split('.')[:-1])
     spec = importlib.util.spec_from_file_location(module_name, file_path)
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
