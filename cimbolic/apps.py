52110dac57502c17ef475e6aa9a42ba46ea3f962
Sharif M. Tarik <s.tarik@cimplux.com>
2019-09-10 20:15:19 +0600

    Add a verbose name for the application

--- cimbolic/apps.py
+++ cimbolic/apps.py
@@ -6,6 +6,7 @@ from .checks import check_for_system_defined_variable_file
 
 class CimbolicConfig(AppConfig):
     name = 'cimbolic'
+    verbose_name = 'Cimbolic Language & Parser'
 
     def ready(self):
         register(check_for_system_defined_variable_file)
