0586088c9b772b0c9ca82bf1a497ab36c0fb460e
Sharif M. Tarik <s.tarik@cimplux.com>
2019-09-11 14:01:40 +0600

    Implement attach_formula_to_variable function

--- cimbolic/utils.py
+++ cimbolic/utils.py
@@ -6,6 +6,7 @@ import lazy_import
 from django.conf import settings
 
 from . import SYSTEM_DEFINED_VARIABLE_FILE_NAME
+from .exceptions import VariableNotFoundError
 
 Variable = lazy_import.lazy_module('cimbolic.models.Variable')
 Formula = lazy_import.lazy_module('cimbolic.models.Formula')
@@ -50,11 +51,26 @@ def create_variable(name: str, source_model: str, description: str = '',
     return var
 
 
-def attach_formula_to_variable(var: Union[str, 'Variable'], formula: Tuple[str, str], priority: int) -> 'Formula':
+def attach_formula_to_variable(var: Union[str, 'Variable'], formula: Tuple[str, str],
+                               priority: int, is_active: bool = True) -> 'Formula':
     """Create and attach a formula to the given variable."""
-    pass
+    if isinstance(var, str):
+        try:
+            variable = Variable.objects.get(name=clean_variable_name(var))
+        except Variable.DoesNotExist:
+            raise VariableNotFoundError(f'Variable {var} does not exist in the database')
+    else:
+        variable = var
 
+    condition, rule = formula
+    formula = Formula.objects.create(
+        variable=variable,
+        condition=condition,
+        rule=rule,
+        priority=priority,
+        is_active=is_active
+    )
+    return formula
 
-# TODO: Complete interfaces for variable creation.
 # TODO: Create interfaces for variable maintenance and deletion.
 # TODO: Add the interfaces to __init__.py
