d073eeae28f8f466ee8e93952901b43e34b704ea
Sharif M. Tarik <s.tarik@cimplux.com>
2019-09-17 17:43:50 +0600

    Update the manifest and setup script

--- setup.py
+++ setup.py
@@ -1,38 +1,43 @@
-import os
-from setuptools import setup
+from os import path
+from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-    README = readme.read()
+# See https://github.com/pypa/sampleproject/blob/master/setup.py for details.
 
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+root_directory = path.abspath(path.dirname(__file__))
+with open(path.join(root_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+with open(path.join(root_directory, 'VERSION.txt'), encoding='utf-8') as f:
+    version = f.read().strip()
 
 setup(
-    name='Cimbolic',
-    version='0.1.0',
-    packages=['cimbolic', 'cimbolic.parsers', 'cimbolic.migrations'],
-    include_package_data=True,
-    url='https://dev.azure.com/Cimplux/CimbolicParser',
-    author='A. G. M. Imam Hossain;  Sharif M. Tarik',
-    author_email='imam.hossain@cimplux.com; s.tarik@cimplux.com',
+    name='cimbolic',
+    version=version,
     description='Cimbolic Language & Parser',
-    long_description=README,
-    install_requires=[
-        'pyparsing==2.4.2',
-    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://dev.azure.com/Cimplux/CimbolicParser',
+    author='Cimplux',
+    author_email='info@cimplux.com',
     classifiers=[
+        'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
         'Intended Audience :: Developers',
         'License :: Other/Proprietary License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Text Processing',
         ],
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=['pyparsing >=2.4'],
+    package_data={'cimbolic': ['management/commands/*']},
+    project_urls={
+        'Source': 'https://dev.azure.com/Cimplux/_git/CimbolicParser',
+    },
 )
