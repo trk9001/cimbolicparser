c4979738932cf48338524a789d964c4af0aa61fe
Sharif M. Tarik <s.tarik@cimplux.com>
2019-09-24 12:53:33 +0600

    Fix incorrect import in custom system check
    (Fixes #238)

--- cimbolic/checks.py
+++ cimbolic/checks.py
@@ -1,7 +1,7 @@
 import os.path
 
 from django.conf import settings
-from django.core.checks import ERROR
+from django.core.checks import Error
 
 from . import SYSTEM_DEFINED_VARIABLE_FILE_NAME
 
@@ -12,9 +12,9 @@ def check_for_system_defined_variable_file(app_configs, **kwargs):
     file_path = os.path.join(settings.BASE_DIR, SYSTEM_DEFINED_VARIABLE_FILE_NAME)
     if not os.path.exists(file_path):
         errors.append(
-            ERROR(
-                'The file in which system-defined variables are stored wasn\'t found',
-                hint=f'Make sure the {SYSTEM_DEFINED_VARIABLE_FILE_NAME} file exists in the project\'s root directory',
+            Error(
+                "The file in which system-defined variables are stored wasn't found",
+                hint=f"Make sure the {SYSTEM_DEFINED_VARIABLE_FILE_NAME} file exists in the project's root directory",
                 id='cimbolic.E001',
             )
         )
